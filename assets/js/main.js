37b2fd30ef2abbe799df393f38cbd5e4084d464c
salvoab <salvatore.abate85@gmail.com>
2021-01-11 15:03:42 +0100

    Layout della pagina

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+/***************************************************************************************************
+Attraverso una chiamata ajax all’Api di boolean avremo a disposizione una decina di dischi musicali. 
+Servendoci di Vue JS stampiamo tutto a schermo.
+
+Chiamata: https://flynn.boolean.careers/exercises/api/array/music
+****************************************************************************************************/
+
+let app = new Vue({
+    
+});
